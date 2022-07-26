e06fe4a91f5543931e6eaea6b95af78dddc50c2e
Elena Denezhnaia <elenamelicenta@gmail.com>
2022-07-26 13:25:55 +0300

    configs updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SkyeStikers'`, () => {
+  it(`should have as title 'SkyeStickers'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SkyeStikers');
+    expect(app.title).toEqual('SkyeStickers');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SkyeStikers!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SkyeStickers!');
   });
 });
